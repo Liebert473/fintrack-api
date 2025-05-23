ebfdb1a232564541142bde42d857898119e974e6
Jhon Williams <prokhamexe@gmail.com>
2025-05-23 15:45:54 +0630

    Auth update

---
+++ auth/RegisterRoute.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const router = express.Router()
+import connectDB from '../db.js';
+
+router.post('/api/auth/register', async (req, res) => {
+    const { email, password, name, username } = req.body
+    if (!email || !password || !name || !username) return res.status(400).json({ error: 'Some reqired information are missing.' })
+
+    const db = await connectDB()
+    const user = await db.collection('users').findOne({ email })
+    if (user) return res.status(409).json({ error: 'User already exists.' })
+
+    const passwordHash = await bcrypt.hash(password, 10)
+    const result = await db.collection('users').insertOne({
+        email,
+        username,
+        name,
+        passwordHash,
+        createdAt: new Date()
+    })
+
+    const token = jwt.sign({ userId: result.insertedId }, process.env.JWT_SECRET, { expiresIn: '7d' })
+    res.json({ token })
+})
+
+export default router
