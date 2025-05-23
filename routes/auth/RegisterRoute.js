c0ab6fb8fb701b994c47550e528ebd397c98484a
Jhon Williams <prokhamexe@gmail.com>
2025-05-23 16:21:56 +0630

    minor fix

--- routes/auth/RegisterRoute.js
+++ routes/auth/RegisterRoute.js
@@ -12,9 +12,10 @@ router.post('/api/auth/register', async (req, res) => {
     if (!email || !password || !name || !username) return res.status(400).json({ error: 'Some reqired information are missing.' })
 
     const db = await connectDB()
-    const user = await db.collection('users').find({ email, username })
+    const user = await db.collection('users').find({ email, username }).toArray()
+
+    if (user.length > 0) return res.status(409).json({ error: 'User already exists.' })
     console.log(user)
-    if (user) return res.status(409).json({ error: 'User already exists.' })
 
     const passwordHash = await bcrypt.hash(password, 10)
     const result = await db.collection('users').insertOne({
